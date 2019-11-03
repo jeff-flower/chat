bf0dae7be93a6e8333f5abcb47066d7f08b19c41
Jeff Flower <jeff.flower14@gmail.com>
2019-11-03 10:53:20 -0700

    Style the message list

--- client/src/components/Messenger/MessageList.tsx
+++ client/src/components/Messenger/MessageList.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import {Message} from './Messenger';
 
+import './MessageList.css';
+
 export const MessageList: React.FC<{messages: Message[]}> = ({messages}) => (
   <div>
     <h2 id="messages">Messages</h2>
-    <ul aria-labelledby="messages">
+    <ul aria-labelledby="messages" className="message-list">
       {messages.map(message => (
-        <li key={message.id}>
+        <li key={message.id} className="message-list__message">
           {`${message.from}: ${message.text}`}
         </li>
       ))}
