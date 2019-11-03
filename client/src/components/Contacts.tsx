84dab79fe91a600956539bbba3e8d523ffbcf245
Jeff Flower <jeff.flower14@gmail.com>
2019-11-02 23:39:54 -0600

    Associate label with lists for a11y best practice

--- client/src/components/Contacts.tsx
+++ client/src/components/Contacts.tsx
@@ -4,8 +4,8 @@ import {User} from './AppContainer';
 
 export const Contacts: React.FC<{contacts: User[], onContactSelection: (contactName: string) => void}> = ({contacts, onContactSelection}) => (
   <section>
-    <h2>Contacts</h2>
-    <ul>
+    <h2 id="contacts">Contacts</h2>
+    <ul aria-labelledby="contacts">
       {contacts.map(contact => <li key={contact.username}><button onClick={() => onContactSelection(contact.username)}>{contact.username}</button></li>)} 
     </ul>
   </section>
--- client/src/components/Messenger/MessageList.tsx
+++ client/src/components/Messenger/MessageList.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import {Message} from './Messenger';
 
 export const MessageList: React.FC<{messages: Message[]}> = ({messages}) => (
-  <ul>
-    {messages.map(message => (
-      <li key={message.id}>
-        {`${message.from}: ${message.text}`}
-      </li>
-    ))}
-  </ul>
+  <div>
+    <h2 id="messages">Messages</h2>
+    <ul aria-labelledby="messages">
+      {messages.map(message => (
+        <li key={message.id}>
+          {`${message.from}: ${message.text}`}
+        </li>
+      ))}
+    </ul>
+  </div>
 );
