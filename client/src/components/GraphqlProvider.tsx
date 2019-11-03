1e425d2b3bbb4ecf3709ea9ce7e1f43794033cb9
Jeff Flower <jeff.flower14@gmail.com>
2019-11-02 22:50:28 -0600

    Remove unused imports

--- client/src/components/GraphqlProvider.tsx
+++ client/src/components/GraphqlProvider.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { split } from 'apollo-link';
 import { WebSocketLink } from 'apollo-link-ws';
