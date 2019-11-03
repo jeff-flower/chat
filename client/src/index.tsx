c94f4a59772a549221b21b126512dcd4fc796eff
Jeff Flower <jeff.flower14@gmail.com>
2019-11-02 21:23:39 -0600

    Move components to a components folder

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {GraphqlProvider} from './GraphqlProvider';
-import App from './App';
+import {GraphqlProvider} from './components/GraphqlProvider';
+import App from './components/App';
 
 const Root = (<GraphqlProvider><App /></GraphqlProvider>);
 
